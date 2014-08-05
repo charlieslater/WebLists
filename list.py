1fa6c93e47226221330a331ca51382b0c77ad578
lmp <lauren.m.patterson@gmail.com>
2014-08-05 15:16:01 -0400

    fixed jinja

--- list.py
+++ list.py
@@ -1,63 +1,92 @@
 import webapp2
 import cgi 
 import urllib
+import os
+import jinja2
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
 
 MAIN_PAGE_FOOTER_TEMPLATE = """\
-    <form action="/write?%s" method="post">
-      <div><textarea name="content" rows="1" cols"60"></textarea></div>
+    <form action="/write?%(list_query_params)s" method="post">
+      <div><textarea type="checkbox" name="content" rows="1" cols"60"></textarea></div>
       <div><input type="submit" value="Add to List"></div>
     </form>
     <hr>
     <form> List name:
-      <input value"%s" name="list_name">
+      <input value"%(list_name)s" name="list_name">
       <input type="submit" value="switch">
     </form>
-    <a href="%s">%s</a>
+    <a href="%(url)s">%(url_link)s</a>
   </body>
 </html>
 """
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        list_name = self.request.get('list_name', DEFAULT_LIST_NAME)
+        print 'list name=', list_name
+        listcontents_query = ListContent.query(
+            ancestor=list_key(list_name)).order(-ListContent.date)
+        listcontents = listcontents_query.fetch(10)
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout' 
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+        list_query_params = urllib.urlencode({'list_name': list_name})
+        template_values = {"listcontents": listcontents, "list_query_params":list_query_params, "list_name":cgi.escape(list_name),
+                           "url":url, "url_link": url_linktext}         
+          
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
 
 DEFAULT_LIST_NAME = 'default_list'
 
 def list_key(list_name=DEFAULT_LIST_NAME):
     return ndb.Key('List', list_name)
 
+
 class ListContent(ndb.Model):
     author = ndb.UserProperty()
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
-class MainPage(webapp2.RequestHandler): 
+class oldMainPage(webapp2.RequestHandler): 
     def get(self):
         self.response.write('<html><body>')
         list_name = self.request.get('list_name', DEFAULT_LIST_NAME)
-        ListContent_query = ListContent.query(
+        listcontents_query = ListContent.query(
             ancestor=list_key(list_name)).order(-ListContent.date)
-        ListContents = ListContent_query.fetch(10)
-        for c in ListContents:
+        listcontents = listcontents_query.fetch(10)
+         
+        for c in listcontents:
             if c.author:
-                self.response.write(
-                       '<b>%s</b> wrote:' % ListContents.author.nickname())
+                self.response.write('<b>%s</b> added:' % c.author.nickname())
             else:
-                self.response.write('Anonymous added:')
+                self.response.write('anonymous added:')
             self.response.write('<blockquote>%s</blockquote>' % 
                                 cgi.escape(c.content))
 
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Login' 
+            url_linktext = 'Logout' 
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
-        sign_query_params = urllib.urlencode({'list_name': list_name})
-        self.response.write(MAIN_PAGE_FOOTER_TEMPLATE % 
-                           (sign_query_params, cgi.escape(list_name), 
-                           url, url_linktext))
+        list_query_params = urllib.urlencode({'list_name': list_name})
+
+        self.response.write(MAIN_PAGE_FOOTER_TEMPLATE %
+                           {"list_query_params":list_query_params, "list_name":cgi.escape(list_name),
+                           "url":url, "url_link": url_linktext})         
         
-class List(webapp2.RequestHandler):
+class Lists(webapp2.RequestHandler):
     def post(self):
         list_name = self.request.get('list_name', 
                                      DEFAULT_LIST_NAME)
@@ -73,6 +102,6 @@ class List(webapp2.RequestHandler):
 
 application = webapp2.WSGIApplication([
     ('/', MainPage), 
-    ('/write', List), 
+    ('/add', Lists), 
 ], debug=True)
 
